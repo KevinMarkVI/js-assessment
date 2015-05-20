cc22c474ec8e09c0ecce3f81518b9012e4b33ba5
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-05-19 18:51:55 -0700

    Array progress

--- app/arrays.js
+++ app/arrays.js
@@ -2,36 +2,66 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    indexOf : function(arr, item) {
 
+    indexOf : function(arr, item) {
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+          return i;
+        }
+      }
     },
 
     sum : function(arr) {
-
+      var sum = 0;
+      for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+      }
+      return sum;
     },
 
     remove : function(arr, item) {
-
+      var indexArray = [];
+      for (var i = arr.length-1; i <= 0; i--) {
+        if (arr[i] === item) {
+          indexArray.push(i);
+        }
+      }
+      for (var j = 0; j < indexArray.length; j++) {
+        arr.splice(indexArray[j], 1);
+      }
+      return arr;
     },
 
     removeWithoutCopy : function(arr, item) {
-
+      var indexArray = [];
+      for (var i = arr.length-1; i <= 0; i--) {
+        if (arr[i] === item) {
+          indexArray.push(i);
+        }
+      }
+      for (var j = 0; j < indexArray.length; j++) {
+        arr.splice(indexArray[j], 1);
+      }
+      return arr;
     },
 
     append : function(arr, item) {
-
+      arr.push(item);
+      return arr;
     },
 
     truncate : function(arr) {
-
+      arr.pop();
+      return arr;
     },
 
     prepend : function(arr, item) {
-
+      arr.unshift(item);
+      return arr;
     },
 
     curtail : function(arr) {
-
+      
     },
 
     concat : function(arr1, arr2) {
